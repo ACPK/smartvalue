e9558bb4f84a64a05b26d8c2b2520a86c9bec771
Laurent Curau <laurentcurau@gmail.com>
2013-12-13 03:12:21 -0600

    Remove price column (replaced with stock_price)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213065629) do
+ActiveRecord::Schema.define(version: 20131213090356) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20131213065629) do
     t.integer  "cash_flow_per_share"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "price"
     t.integer  "stock_price"
   end
 
